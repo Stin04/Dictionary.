1fe15c062d947920e5d3d6913ce843bd980e0905
Stin04 <justineidos04@gmail.com>
2025-02-14 22:58:59 +0800

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,41 @@
+# JUSTINE IDOS 12-CPE-01
+# DICTIONARY IN PYDROID
+# Ml HERO VOICE 
+
+DICTIONARY = {
+
+"Beatrix": "If sheer words could break you, what cannot",
+ "Alucard": "Nothing last forever, we can change the future",
+"Chou": "Not knowing oneself, that’s the 			worst", 
+ "Lunox": "Light and dark, heads and tails of one coin", 
+"Vexana": "There are memories time does 		not erase",
+"Selena": "When you gaze into the Abyss… The Abyss Gazes back",
+"Hayabusa": "Your inner monster is your first 	enemy",
+"Argus": "The shadow in my heart is deeper   	than the abyss",
+"Pharsa": "My world, is the sky", 
+"Xavier": "This time the choice is mine",
+"Erithel": "Lets go",
+"Fanny": "Sir whats your command",
+"Yin": "i am yin and always will be no matter        what",
+"Nana": "Do you want to make friends with nana", 
+"Layla": "Ready Go",
+"Digie": "A star will disappear if you lose",
+"Natan": "It is in despair that i see the fondest hopes",
+"Alpha": "What are you to me beta",
+"Saber":"Im a man with sad stories",
+"Cecelion": "Living forever is not a blessing, but curse",
+"Pharsa": "The higher you fly, the more you see","Martis": "Who here is worthy",
+"Hanzo": "Weaking should down in blood",
+"Moscov": "I am vengeance incarnate",
+"Alpha": "Breakable beings",
+"Martis":"Three thousand words and not a single worthy foe",
+"Hanabi": "Move on Thats all you can do",
+"Terizla": "History has long forgotten me",
+"Khufra": "The word is mine, just for you",
+"Carmila": "True love is like blood",
+
+  }
+
+a = input ("Enter hero name:")
+
+print (DICTIONARY [a])
